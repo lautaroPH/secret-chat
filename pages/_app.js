3ad088985465d3a8b1e98af80001edcfc6252589
lautaroPH <lautaroph@gmail.com>
2022-07-09 21:30:55 -0300

    remove matrixCard in index and add to the app

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import { ActiveConversation } from 'context/ConversationContext';
 import '../styles/globals.css';
 import { useState } from 'react';
+import MatrixCard from 'matrix-card';
 
 function MyApp({ Component, pageProps }) {
   const [activeConversation, setActiveConversation] = useState('');
@@ -9,7 +10,28 @@ function MyApp({ Component, pageProps }) {
     <ActiveConversation.Provider
       value={{ activeConversation, setActiveConversation }}
     >
-      <Component {...pageProps} />
+      <MatrixCard
+        id={'my-id-1'}
+        matrixText={`abcdefghijklmnopqrstuvwxyz0123456789$+-*/=%"'#&_(),.;:?!\\|{}<>[]^~`}
+        delay={40}
+        backgroundColor={'rgba(10, 10, 10, 0.2)'}
+        textFontSize={'16'}
+        textMainColor={'#0c0'}
+        textAlternateColorRatio={0.1}
+        textAlternateColorList={['#080', '#090', '#0a0', '#0b0']}
+        styleOverrideForContainerDiv={{
+          backgroundColor: 'rgb(10, 10, 10)',
+        }}
+        styleOverrideForChildrenDiv={{
+          backgroundColor: 'transparent',
+          top: '0%',
+          left: '0%',
+          width: '100%',
+          height: '100%',
+        }}
+      >
+        <Component {...pageProps} />
+      </MatrixCard>
     </ActiveConversation.Provider>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,34 @@
-import Form from 'components/FormRoom/Form';
 import ButtonsLogin from 'components/Login/ButtonsLogin';
 import UserHead from 'components/UserHead/UserHead';
-import MatrixCard from 'matrix-card';
 import useUser from 'hooks/useUser';
+import Title from 'components/Title';
+import StartChat from 'components/FormRoom/StartChat';
+import JoinChat from 'components/FormRoom/JoinChat';
+import { useState } from 'react';
 
 export default function Home() {
   const user = useUser();
+  const [buttonActive, setButtonActive] = useState(false);
 
   return (
-    <div className="relative flex flex-col items-center">
-      <MatrixCard
-        id={'my-id-1'}
-        matrixText={`abcdefghijklmnopqrstuvwxyz0123456789$+-*/=%"'#&_(),.;:?!\\|{}<>[]^~`}
-        delay={40}
-        backgroundColor={'rgba(42, 40, 45, 0.2)'}
-        textFontSize={'16'}
-        textMainColor={'#0c0'}
-        textAlternateColorRatio={0.1}
-        textAlternateColorList={['#080', '#090', '#0a0', '#0b0']}
-        styleOverrideForContainerDiv={{
-          backgroundColor: 'rgba(42, 40, 45)',
-        }}
-        styleOverrideForChildrenDiv={{
-          backgroundColor: 'transparent',
-          top: '0%',
-          left: '0%',
-          width: '100%',
-          height: '100%',
-        }}
-      >
-        <h1 className="mt-5 text-center text-8xl">Secret Chat</h1>
-        {user == null && <ButtonsLogin />}
-        {user && (
-          <div>
-            <UserHead />
-
-            <Form />
-          </div>
-        )}{' '}
-      </MatrixCard>
-    </div>
+    <>
+      {user && <UserHead user={user} />}
+      <div className="flex justify-center mt-16">
+        <Title />
+      </div>
+      {user == null && <ButtonsLogin />}
+      {user && (
+        <div className="flex flex-col justify-center gap-10 mt-20">
+          <StartChat
+            setButtonActive={setButtonActive}
+            buttonActive={buttonActive}
+          />
+          <JoinChat
+            setButtonActive={setButtonActive}
+            buttonActive={buttonActive}
+          />
+        </div>
+      )}{' '}
+    </>
   );
 }
