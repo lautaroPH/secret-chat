cb70893c617bfbc20765e761f2e5393c3e9ef358
lautaroPH <lautaroph@gmail.com>
2022-07-09 21:27:08 -0300

    add title component

---
+++ components/Title.js
@@ -0,0 +1,9 @@
+const Title = () => {
+  return (
+    <h1 className="block overflow-hidden whitespace-nowrap border-r-4 border-green-code w-[12ch] text-5xl  text-green-code titleAnimation">
+      Secret Chat.
+    </h1>
+  );
+};
+
+export default Title;
