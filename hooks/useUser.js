4f91da0a610913fc107e24263f6d8c20535752f6
lautaroPH <lautaroph@gmail.com>
2022-07-16 16:51:42 -0300

    change initial state

--- hooks/useUser.js
+++ hooks/useUser.js
@@ -2,8 +2,8 @@ import useSession from './useSession';
 import { useState, useEffect } from 'react';
 
 const useUser = () => {
-  const session = useSession(false);
-  const [user, setUser] = useState(session?.user);
+  const session = useSession();
+  const [user, setUser] = useState(false);
   useEffect(() => setUser(session?.user), [session]);
 
   return user;
