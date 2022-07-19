4f8ff75d0f5c83e004e7e49b86b97a50adf75122
lautaroPH <lautaroph@gmail.com>
2022-07-19 09:29:25 -0300

    add more messageErrors

--- utils/errorsMessages.js
+++ utils/errorsMessages.js
@@ -1,10 +1,14 @@
 export const errorsMessages = (err) => {
-  if (err === 'Conflict') {
+  if (err === 'Conflict room') {
     return 'Room already exists';
-  } else if (err === 'Not Found') {
+  } else if (err === 'Not Found room' || err === 'Not Found accessRoom') {
     return "Room doesn't exist";
-  } else if (err === 'Forbidden') {
+  } else if (err === 'Forbidden room' || err === 'Forbidden accessRoom') {
     return "You don't have permission to access this room";
+  } else if (err === 'Conflict addUser') {
+    return 'User already exists in the chat';
+  } else if (err === 'Bad Request addUser') {
+    return "User doesn't exist";
   }
   return err;
 };
