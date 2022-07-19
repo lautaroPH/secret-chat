8a94eeb7481c1d9718e0dd8a68191239bfbf7409
lautaroPH <lautaroph@gmail.com>
2022-07-19 09:31:44 -0300

    add swalComponent

--- components/UserHead/ButtonLogout.js
+++ components/UserHead/ButtonLogout.js
@@ -1,3 +1,5 @@
+import { swalLogoutError } from 'Swals/SwalLogoutError';
+import Swal from 'sweetalert2';
 import { supabaseClient } from 'utils/supabaseClient';
 
 const ButtonLogout = () => {
@@ -5,7 +7,7 @@ const ButtonLogout = () => {
     const { error } = await supabaseClient.auth.signOut();
 
     if (error) {
-      alert(error);
+      Swal.fire(swalLogoutError(error.message));
     }
   };
 
