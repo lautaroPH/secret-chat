8066f315d339801fe00e34914a2d5f4c24e6232a
lautaroPH <lautaroph@gmail.com>
2022-07-19 09:29:34 -0300

    add createUser

---
+++ utils/createUser.js
@@ -0,0 +1,24 @@
+import { swalErrors } from 'Swals/SwalErrors';
+import Swal from 'sweetalert2';
+import { supabaseClient } from './supabaseClient';
+
+export const createUser = async (user) => {
+  const { data: profile } = await supabaseClient
+    .from('profiles')
+    .select('*')
+    .eq('id', user.id);
+  console.log(profile);
+  if (!profile || profile.length === 0) {
+    const { error } = await supabaseClient.from('profiles').insert([
+      {
+        id: user.id,
+        userName: user.user_metadata.user_name,
+        image: user.user_metadata.avatar_url,
+      },
+    ]);
+
+    if (error) {
+      Swal.fire(swalErrors(`${error.message} createUser`));
+    }
+  }
+};
