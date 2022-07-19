96aaed11ff77a9946e08329e727fe4644cca064f
lautaroPH <lautaroph@gmail.com>
2022-07-19 09:39:04 -0300

    add isAdmin

---
+++ components/ChatOptions/IsAdmin.js
@@ -0,0 +1,9 @@
+const IsAdmin = () => {
+  return (
+    <p className="text-xs tracking-widest text-green-code border border-green-code p-[3px]">
+      Admin
+    </p>
+  );
+};
+
+export default IsAdmin;
